0ffc60ad024e55e106c950ccbea60290f18727ef
christian <christian.peters@gmail.com>
2022-06-16 09:38:48 -0500

    Start towards nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './clients/nav-bar/nav-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent
-  ],
+  declarations: [AppComponent, NavBarComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/clients/nav-bar/nav-bar.component.ts
+++ src/app/clients/nav-bar/nav-bar.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import {LinksAccessorService} from "../../accessors/links-accessor.service";
+import { LinksAccessorService } from '../../accessors/links-accessor.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent implements OnInit {
-  constructor(private service: LinksAccessorService) { }
+  organizationNames: string[] = [];
+  activeLink: string = '';
+
+  constructor(private service: LinksAccessorService) {}
 
   ngOnInit(): void {
-    this.service.$links.subscribe(l=> console.log(l));
+    this.service.$links.subscribe((o) => {
+      this.organizationNames = o.map((org) => org.name);
+    });
   }
-
 }
